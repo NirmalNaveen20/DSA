00b776ef3fad79be843a2fc9643879978c7e78a3
Nirmal Naveen <53178630+NirmalNaveen20@users.noreply.github.com>
2022-11-05 19:13:05 -0700

    Add files via upload

---
+++ Quick Sort.py
@@ -0,0 +1,17 @@
+A=[]
+for v in range (6):
+    A.append(int(input('Enter a number : ')))
+print (A)
+
+def selectionSort(A):
+    n= len(A)
+    for j in range (0,n-1):
+        smallest = j
+        for i in range (j+1,n):
+            if A[i] <A[smallest]:
+                smallest = i
+        A[j],A[smallest] = A[smallest],A[j]
+
+selectionSort(A)
+print('Sorted Array')
+print(A)
